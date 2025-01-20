10d8c67fd163a949e5b1e6454e37ac742ca53a17
Chí Công <cong.pc2305@gmail.com>
2025-01-20 23:36:25 +0700

    update

--- log_archive_tool/cronjob.sh
+++ log_archive_tool/cronjob.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-SCRIPT_PATH="./log-archive.sh"
-LOG_FOLDER_PATH="./logs"
+BASEDIR="$(
+    cd -- "$(dirname "$0")" >/dev/null 2>&1
+    pwd -P
+)"
+SCRIPT_PATH="${BASEDIR}/log-archive.sh"
+LOG_FOLDER_PATH=$1
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <log-directory>"
+    LOG_FOLDER_PATH="${BASEDIR}/logs"
+    echo "Default log directory is setted ${LOG_FOLDER_PATH}"
+fi
 
 create_cronjob() {
     cron_line="0 * * * * ${SCRIPT_PATH} ${LOG_FOLDER_PATH}"
